cd8ee11d0eeaf14a62a4e64c03f11a1454fa911a
Iván Monte <ivanmonte07.93@gmail.com>
2025-02-08 11:47:57 -0300

    fixing errors in types data

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,37 +1,52 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { CustomerField } from '@/app/lib/definitions';
 import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
 
-interface PageProps {
-  params: { id: string };
-}
+export const metadata: Metadata = {
+    title: 'Edit an Invoice',
+};
 
-export default async function Page({ params }: PageProps) {
-  const id = params.id;
 
-  const [invoice, customers] = await Promise.all([
-    fetchInvoiceById(id),
-    fetchCustomers(),
-  ]);
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+    // Await the `params` before destructuring
+    const params = await props.params;
+    const id = params.id;
 
-  if (!invoice) {
-    notFound();
-  }
+    // Fetch data in parallel
+    const [customersData] = await Promise.all([
+        //fetchInvoiceById(id),
+        fetchCustomers(),
+    ]);
 
-  return (
-    <main>
-      <Breadcrumbs
-        breadcrumbs={[
-          { label: 'Invoices', href: '/dashboard/invoices' },
-          {
-            label: 'Edit Invoice',
-            href: `/dashboard/invoices/${id}/edit`,
-            active: true,
-          },
-        ]}
-      />
-      <Form invoice={invoice} customers={customers} />
-    </main>
-  );
+    // Transform customersData to match `CustomerField` type
+    const customers: CustomerField[] = customersData.map((customer: any) => ({
+        id: customer.id,
+        name: customer.name,
+    }));
+
+
+    const invoice: any = await fetchInvoiceById(id);
+
+    if (!invoice) {
+        notFound();
+    }
+
+    return (
+        <main>
+            <Breadcrumbs
+                breadcrumbs={[
+                    { label: 'Invoices', href: '/dashboard/invoices' },
+                    {
+                        label: 'Edit Invoice',
+                        href: `/dashboard/invoices/${id}/edit`,
+                        active: true,
+                    },
+                ]}
+            />
+            <Form invoice={invoice} customers={customers} />
+        </main>
+    );
 }
